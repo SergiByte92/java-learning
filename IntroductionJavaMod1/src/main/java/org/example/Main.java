1a580a346c3efe91797e91342e7aebc5188ab9c8
SergiByte92 <sergigs92@gmail.com>
2026-05-22 12:21:46 +0200

    Add initial Java Maven module

---
+++ IntroductionJavaMod1/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+/*
+
+Introduction to Java and Object-Oriented Programming
+Modulo 1
+
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola Java");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hola número " + i);
+        }
+
+        String java = "Java mola";
+        String espacio = "";
+        String aprendiendoJava = "y me gusta más que c#";
+
+        String line = java + espacio + aprendiendoJava;
+
+        System.out.println(line);
+
+        System.out.println(11);
+
+    }
+}
